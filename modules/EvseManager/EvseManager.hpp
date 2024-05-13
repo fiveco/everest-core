a2fe0945c6c6139f1927dc674242cac5d7030c47
Antoine Gardiol <antoine.gardiol@fiveco.ch>
2024-05-13 17:24:27 +0200

    Disable autocharge by default

--- modules/EvseManager/EvseManager.hpp
+++ modules/EvseManager/EvseManager.hpp
@@ -79,6 +79,7 @@ struct Conf {
     bool hack_pause_imd_during_precharge;
     bool hack_allow_bpt_with_iso2;
     bool autocharge_use_slac_instead_of_hlc;
+    bool enable_autocharge;
     std::string logfile_suffix;
     double soft_over_current_tolerance_percent;
     double soft_over_current_measurement_noise_A;
